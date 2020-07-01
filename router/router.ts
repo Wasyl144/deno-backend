6ebe613c01d115d637b1a4f1fecb10da89534fff
michał frankiewicz <wasyl144@outlook.com>
2020-07-01 14:48:55 +0200

    Added delete method and put method, backend works

--- router/router.ts
+++ router/router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
-import { getProducts, getProduct, delProduct } from '../controller/controller.ts';
+import { getProducts, getProduct, delProduct, addProduct, updateProduct } from '../controller/controller.ts';
 const router = new Router()
 
 router.get('/api/list', getProducts)
   .get('/api/list/:id', getProduct)
   .delete('/api/del/:id', delProduct)
+  .post('/api/add', addProduct)
+  .put('/api/update/:id', updateProduct)
 
 export default router
